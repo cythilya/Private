cbf92da92a8d882f8f46aa4e65d3ea5191e889bf
Cythilya Tang <cythilya@gmail.com>
2014-09-10 10:34:04 +0800

    Ember JS & SEO

---
+++ cythilya/cythilya/Areas/SEOLab/SEOLabAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace cythilya.Areas.SEOLab
+{
+    public class SEOLabAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "SEOLab";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "SEOLab_default",
+                "SEOLab/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
