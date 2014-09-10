ef690ad7494cf33d1638a98437a2b7f84b3774ca
Cythilya Tang <cythilya@gmail.com>
2014-09-10 11:35:06 +0800

    Ember JS & SEO

--- cythilya/cythilya/Content/seo_lab/js/script.js
+++ cythilya/cythilya/Content/seo_lab/js/script.js
@@ -42,7 +42,7 @@ ProductList.Route = Ember.Route.extend({
 ProductList.Product.FIXTURES = [
 	{
 		id: 1,
-		no: 100,
+		no: 3501,
 		cover: "http://friendoprod.blob.core.windows.net/missionpics/images/main/240b0e00-72f7-4cca-a5ef-dbd4646efc2d.gif",
 		name: '<a href="http://www.friendo.com.tw/Mission/3501" target="_blank">九品芝麻官大會考</a>',
 		category: "相信有不少人都看過《九品芝麻官》，電影中滿滿的「老木」不說，妓院老鴇就佔了一大篇幅，貪官描述更是不遺餘力，可以說是一部藉古諷今的電影。電影中也有不少經典台詞及橋段，現在就來考考特務們對這部電影的記憶力吧！",
@@ -50,7 +50,7 @@ ProductList.Product.FIXTURES = [
 	},
 	{
 		id: 2,
-		no: 101,
+		no: 3521,
 		cover: "http://friendoprod.blob.core.windows.net/missionpics/images/main/dfe4e330-ab91-41c6-834a-d22264adcf42.gif",
 		name: '<a href="http://www.friendo.com.tw/Mission/3521" target="_blank">與龍共舞隨堂考</a>',
 		category: "每次只要轉到港劇<<與龍共舞>>，就算早已看過上百遍，還是會忍不住停下來看，然後被老梗再次逗得哈哈大笑，其中最有名的梗當然就屬流行20年以上的「龍蝦裝」了。<<與龍共舞>>真是一部百看不膩的電影，現在就來考考特務們對與龍共舞的熟悉度吧！",
@@ -58,7 +58,7 @@ ProductList.Product.FIXTURES = [
 	},
 	{
 		id: 3,
-		no: 102,
+		no: 3556,
 		cover: "http://friendoprod.blob.core.windows.net/missionpics/images/main/bc2fae4f-dc2e-4d31-b6fd-8c9b3870aef6.jpg",
 		name: '<a href="http://www.friendo.com.tw/Mission/3556" target="_blank">少林足球隨堂考</a>',
 		category: "搜尋引擎提供使用者想要的資訊，而社群平台提供這些資訊更多露出的機會。我們希望將優質的內容提供給使用者，並利用社群的力量推廣出去。...",
@@ -66,7 +66,7 @@ ProductList.Product.FIXTURES = [
 	},
 	{
 		id: 4,
-		no: 103,
+		no: 3485,
 		cover: "http://friendoprod.blob.core.windows.net/missionpics/images/main/045c1eb9-5708-498c-9d4a-61db3d2f4952.jpg",
 		name: '<a href="http://www.friendo.com.tw/Mission/3485" target="_blank">古惑仔大會考</a>',
 		category: "《古惑仔》系列電影為香港著名的黑幫電影，從第一集《古惑仔之人在江湖》上映後便大受歡迎，往後更拍了許多續集，整體氣勢無人能及，堪稱黑幫片的經典！現在就請各位特務們，測驗看看你夠不格當古惑仔吧！",
@@ -74,7 +74,7 @@ ProductList.Product.FIXTURES = [
 	},
 	{
 		id: 5,
-		no: 104,
+		no: 3539,
 		cover: "http://friendoprod.blob.core.windows.net/missionpics/images/main/b1e384c0-d822-4a71-85a1-577abe401615.gif",
 		name: '<a href="http://www.friendo.com.tw/Mission/3539" target="_blank">食神隨堂考</a>',
 		category: "《食神》是喜劇巨星周星馳主演的電影，該片為周星馳重要的代表作之一，在港台各地造成很大影響，其中許多經典台詞，更成為年輕人聊天或搞笑時經常使用的詞語。各位星爺迷們來測驗看看你對星爺有多喜愛吧！",
@@ -82,7 +82,7 @@ ProductList.Product.FIXTURES = [
 	},
 	{
 		id: 6,
-		no: 105,
+		no: 3448,
 		cover: "http://friendoprod.blob.core.windows.net/missionpics/images/main/66f35c25-2a45-42c4-b81e-57963cf9168e.jpg",
 		name: '<a href="http://www.friendo.com.tw/Mission/3448" target="_blank">唐伯虎點秋香大會考</a>',
 		category: "《唐伯虎點秋香》是1993年香港喜劇電影。由周星馳主演。該片為周星馳無厘頭喜劇經典之一，而這部電影也一直在電視上不斷的重複重複再重複…，相信星爺迷們一定對劇情及對白瞭若指掌，現在就來考考特務們電影裡的小知識吧！",
