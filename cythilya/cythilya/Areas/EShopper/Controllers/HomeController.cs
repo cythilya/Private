2f3d00815c181f0c4596388d75e7bff135300872
Cythilya Tang <cythilya@gmail.com>
2014-09-22 23:13:54 +0800

    eshopper index, category

--- cythilya/cythilya/Areas/EShopper/Controllers/HomeController.cs
+++ cythilya/cythilya/Areas/EShopper/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using cythilya.Areas.EShopper.Models;
 
 namespace cythilya.Areas.EShopper.Controllers
 {
@@ -11,17 +12,119 @@ namespace cythilya.Areas.EShopper.Controllers
         //首頁
         public ActionResult Index()
         {
-            return View();
+            #region fake data - product category
+            var data = new List<ProductCategory>()
+            {
+                new ProductCategory() { ID = 1, Name = "WOMENS"},
+                new ProductCategory() { ID = 2, Name = "MENS"},
+                new ProductCategory() { ID = 3, Name = "KIDS"},
+                new ProductCategory() { ID = 4, Name = "SPORTSWEAR"},
+                new ProductCategory() { ID = 5, Name = "FASHION"},
+                new ProductCategory() { ID = 6, Name = "HOUSEHOLDS"},
+                new ProductCategory() { ID = 7, Name = "INTERIORS"},
+                new ProductCategory() { ID = 8, Name = "CLOTHING"},
+                new ProductCategory() { ID = 9, Name = "BAGS"},
+                new ProductCategory() { ID = 10, Name = "SHOES"}
+            };
+            #endregion
+
+            return View(data);
         }
 
         //商品列表
-        public ActionResult ProductList()
+        public ActionResult Category()
         {
-            return View();
+            #region fake data - product category
+            var data = new List<ProductCategory>()
+            {
+                new ProductCategory() { ID = 1, Name = "WOMENS"},
+                new ProductCategory() { ID = 2, Name = "MENS"},
+                new ProductCategory() { ID = 3, Name = "KIDS"},
+                new ProductCategory() { ID = 4, Name = "SPORTSWEAR"},
+                new ProductCategory() { ID = 5, Name = "FASHION"},
+                new ProductCategory() { ID = 6, Name = "HOUSEHOLDS"},
+                new ProductCategory() { ID = 7, Name = "INTERIORS"},
+                new ProductCategory() { ID = 8, Name = "CLOTHING"},
+                new ProductCategory() { ID = 9, Name = "BAGS"},
+                new ProductCategory() { ID = 10, Name = "SHOES"}
+            };
+            #endregion
+
+            #region fake data - product
+            var productCategory = new ProductCategory(){ID = 1, Name = "WOMENS"};
+            
+            var tag = new Tag(){ ID = 1, Name = "New Arrival"};
+            var tagList = new List<Tag>();
+            tagList.Add(tag);
+
+            var ProductList = new List<Product>()
+            {
+                new Product()
+                {
+                    ID = 1, 
+                    Name = "Easy Polo Black Edition",
+                    ProductCategory = productCategory,
+                    Description = "",
+                    RawPicUrl = "",
+                    PicLargeUrl = "",
+                    PicMediumUrl = "/Content/eshopper/images/home/product1.jpg",
+                    PicSmallUrl = "",
+                    PublishOn = DateTime.UtcNow,
+                    Brand = "",
+                    BrandInfo = "",
+                    Tags = tagList,
+                    Price = 99,
+                    Amount = 99,
+                    IsSale = false,
+                    IsHigLight = true
+                },
+                new Product()
+                {
+                    ID = 2, 
+                    Name = "Easy Polo Black Edition",
+                    ProductCategory = productCategory,
+                    Description = "",
+                    RawPicUrl = "",
+                    PicLargeUrl = "",
+                    PicMediumUrl = "/Content/eshopper/images/home/product3.jpg",
+                    PicSmallUrl = "",
+                    PublishOn = DateTime.UtcNow,
+                    Brand = "",
+                    BrandInfo = "",
+                    Tags = tagList,
+                    Price = 99,
+                    Amount = 99,
+                    IsSale = false,
+                    IsHigLight = false
+                },
+                new Product()
+                {
+                    ID = 3, 
+                    Name = "Easy Polo Black Edition",
+                    ProductCategory = productCategory,
+                    Description = "",
+                    RawPicUrl = "",
+                    PicLargeUrl = "",
+                    PicMediumUrl = "/Content/eshopper/images/home/product6.jpg",
+                    PicSmallUrl = "",
+                    PublishOn = DateTime.UtcNow,
+                    Brand = "",
+                    BrandInfo = "",
+                    Tags = tagList,
+                    Price = 99,
+                    Amount = 99,
+                    IsSale = true,
+                    IsHigLight = false
+                }
+            };
+            #endregion
+
+            ViewBag.ProductList = ProductList;
+            return View(data);
         }
 
         //商品明細
-        public ActionResult ProductDetail() 
+        public ActionResult Product()
         {
             return View();
         }
--- cythilya/cythilya/Areas/EShopper/Models/Product.cs
+++ cythilya/cythilya/Areas/EShopper/Models/Product.cs
@@ -7,86 +7,83 @@ using System.ComponentModel.DataAnnotations;
 
 namespace cythilya.Areas.EShopper.Models
 {
+    [DisplayName("商品資訊")]
+    [DisplayColumn("Name")]
     public class Product
     {
-        [DisplayName("商品資訊")]
-        [DisplayColumn("Name")]
-        public class ProductItem
-        {
-            //ID
-            [Key]
-            public int ID { get; set; }
+        //ID
+        [Key]
+        public int ID { get; set; }
 
-            //商品名稱
-            [DisplayName("商品名稱")]
-            [Required(ErrorMessage = "請輸入商品名稱")]
-            [MaxLength(60, ErrorMessage="商品名稱不可超過60字")]
-            public string Name { get; set; }
+        //商品名稱
+        [DisplayName("商品名稱")]
+        [Required(ErrorMessage = "請輸入商品名稱")]
+        [MaxLength(60, ErrorMessage = "商品名稱不可超過60字")]
+        public string Name { get; set; }
 
-            //商品類別
-            [DisplayName("商品類別")]
-            [Required]
-            public ProductCategory ProductCategory { get; set; }
+        //商品類別
+        [DisplayName("商品類別")]
+        [Required]
+        public ProductCategory ProductCategory { get; set; }
 
-            //商品介紹
-            [DisplayName("商品介紹")]
-            [Required(ErrorMessage = "請輸入商品介紹")]
-            public string Description { get; set; }
-            
-            //商品圖片
-            //主打圖片原始檔
-            [DisplayName("主打圖片原始檔")]
-            [Required(ErrorMessage = "請輸入主打圖片原始檔")]
-            public string RawPicUrl { get; set; } 
+        //商品介紹
+        [DisplayName("商品介紹")]
+        [Required(ErrorMessage = "請輸入商品介紹")]
+        public string Description { get; set; }
 
-            //主打圖片, 大(內頁 329x380)
-            [DisplayName("主打內頁大圖(329x380)")]
-            [Required(ErrorMessage = "請輸入主打內頁大圖(329x380)")]
-            public string PicLargeUrl { get; set; } 
+        //商品圖片
+        //主打圖片原始檔
+        [DisplayName("主打圖片原始檔")]
+        [Required(ErrorMessage = "請輸入主打圖片原始檔")]
+        public string RawPicUrl { get; set; }
 
-            //主打圖片, 中(首頁 255x381)
-            [DisplayName("主打中圖(首頁 255x381)")]
-            [Required(ErrorMessage = "請輸入主打中圖(首頁 255x381)")]            
-            public string PicMediumUrl { get; set; } 
+        //主打圖片, 大(內頁 329x380)
+        [DisplayName("主打內頁大圖(329x380)")]
+        [Required(ErrorMessage = "請輸入主打內頁大圖(329x380)")]
+        public string PicLargeUrl { get; set; }
 
-            //主打圖片, 小(內頁 85x84)
-            [DisplayName("主打內頁小圖(85x84)")]
-            [Required(ErrorMessage = "請輸入主打內頁小圖(85x84)")]            
-            public string PicSmallUrl { get; set; } 
-            
-            //上架時間
-            [DisplayName("上架時間")]
-            [Description("如果不設定上架時間，則此商品永不上架")]
-            public DateTime? PublishOn { get; set; } 
+        //主打圖片, 中(首頁 255x381)
+        [DisplayName("主打中圖(首頁 255x381)")]
+        [Required(ErrorMessage = "請輸入主打中圖(首頁 255x381)")]
+        public string PicMediumUrl { get; set; }
 
-            //品牌
-            //品牌名稱
-            [DisplayName("品牌名稱")]
-            public string Brand { get; set; }  
-            
-            //品牌簡介
-            [DisplayName("品牌簡介")]
-            public string BrandInfo { get; set; } 
+        //主打圖片, 小(內頁 85x84)
+        [DisplayName("主打內頁小圖(85x84)")]
+        [Required(ErrorMessage = "請輸入主打內頁小圖(85x84)")]
+        public string PicSmallUrl { get; set; }
 
-            //標籤列表
-            [DisplayName("標籤列表")]
-            public List<Tag> Tags { get; set; } 
+        //上架時間
+        [DisplayName("上架時間")]
+        [Description("如果不設定上架時間，則此商品永不上架")]
+        public DateTime? PublishOn { get; set; }
 
-            //價格
-            [DisplayName("價格")]
-            public int Price { get; set; }
+        //品牌
+        //品牌名稱
+        [DisplayName("品牌名稱")]
+        public string Brand { get; set; }
 
-            //數量
-            [DisplayName("數量")]
-            public int Amount { get; set; } 
+        //品牌簡介
+        [DisplayName("品牌簡介")]
+        public string BrandInfo { get; set; }
 
-            //是否出清
-            [DisplayName("是否出清")]
-            public bool IsSale { get; set; } //是否出清
+        //標籤列表
+        [DisplayName("標籤列表")]
+        public List<Tag> Tags { get; set; }
 
-            //是否為特色商品
-            [DisplayName("是否為特色商品")]
-            public bool IsHigLight { get; set; } //是否為特色商品
-        }
+        //價格
+        [DisplayName("價格")]
+        public int Price { get; set; }
+
+        //數量
+        [DisplayName("數量")]
+        public int Amount { get; set; }
+
+        //是否出清
+        [DisplayName("是否出清")]
+        public bool IsSale { get; set; } //是否出清
+
+        //是否為特色商品
+        [DisplayName("是否為特色商品")]
+        public bool IsHigLight { get; set; } //是否為特色商品
     }
 }
