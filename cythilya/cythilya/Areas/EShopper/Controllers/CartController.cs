b7e1a8c5e3420fcd4f02e12ee55be5a6ab00ab33
Cythilya Tang <cythilya@gmail.com>
2014-09-25 22:22:17 +0800

    eshopper cart

--- cythilya/cythilya/Areas/EShopper/Controllers/CartController.cs
+++ cythilya/cythilya/Areas/EShopper/Controllers/CartController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using cythilya.Areas.EShopper.Models;
 
 namespace cythilya.Areas.EShopper.Controllers
 {
@@ -16,7 +17,67 @@ namespace cythilya.Areas.EShopper.Controllers
         //顯示目前購物車的項目
         public ActionResult Index()
         {
-            return View();
+            #region fake data - product
+            var productCategory = new ProductCategory() { ID = 1, Name = "WOMENS" };
+
+            var tag = new Tag() { ID = 1, Name = "New Arrival" };
+            var tagList = new List<Tag>();
+            tagList.Add(tag);
+
+            Product item1 = new Product(){
+                ID = 1,
+                Name = "Easy Polo Black Edition",
+                ProductCategory = productCategory,
+                Description = "",
+                RawPicUrl = "",
+                PicLargeUrl = "",
+                PicMediumUrl = "/Content/eshopper/images/home/product1.jpg",
+                PicSmallUrl = "/Content/eshopper/images/product-details/similar1.jpg",
+                PublishOn = DateTime.UtcNow,
+                Brand = "",
+                BrandInfo = "",
+                Tags = tagList,
+                Price = 99,
+                Amount = 99,
+                IsSale = false,
+                IsHigLight = true            
+            };
+
+            Product item2 = new Product() {
+                ID = 2,
+                Name = "Easy Polo Black Edition",
+                ProductCategory = productCategory,
+                Description = "",
+                RawPicUrl = "",
+                PicLargeUrl = "",
+                PicMediumUrl = "/Content/eshopper/images/home/product3.jpg",
+                PicSmallUrl = "/Content/eshopper/images/product-details/similar2.jpg",
+                PublishOn = DateTime.UtcNow,
+                Brand = "",
+                BrandInfo = "",
+                Tags = tagList,
+                Price = 99,
+                Amount = 99,
+                IsSale = false,
+                IsHigLight = false            
+            };
+            #endregion
+
+            var data = new List<Cart>() 
+            { 
+                new Cart()
+                {
+                    Product = item1,
+                    Amount = 2
+                },
+                new Cart()
+                {
+                    Product = item2,
+                    Amount = 1
+                }
+            };
+
+            return View(data);
         }
 
         //加入購物車
