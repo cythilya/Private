fdf1ed61ae0e4a2566caf8e984f7b57e19b86a4b
Cythilya Tang <cythilya@gmail.com>
2014-09-26 23:20:51 +0800

    social links

---
+++ cythilya/cythilya/Areas/EShopper/Models/Common.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace cythilya.Areas.EShopper.Models
+{
+    public class Common
+    {
+        public class SocialList
+        {
+            public string Name { get; set; }
+            public string Link { get; set; }
+        }
+    }
+}
