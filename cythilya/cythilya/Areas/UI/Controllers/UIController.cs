93ffc88bee9a6327ff4069c3932796888922c9da
summertang <summer.tang@spring-trees.com>
2014-10-13 18:12:30 +0800

    #5 ckeditor exmaple

--- cythilya/cythilya/Areas/UI/Controllers/UIController.cs
+++ cythilya/cythilya/Areas/UI/Controllers/UIController.cs
@@ -8,9 +8,38 @@ namespace cythilya.Areas.UI.Controllers
 {
     public class UIController : Controller
     {
+        public ActionResult CKEditorPreview() 
+        {
+            //get saved html snippet
+            return View();
+        }
+
         public ActionResult CKEditorDemo()
         {
+            //get editor content
+            //string output = "<div>ckeditor test</div>";//fake (raw) data here
+            //string str = HttpUtility.UrlDecode(output);//decode
+            //ViewBag.Out = str;
             return View();
+        }
+
+        //save editor content
+        public ActionResult SaveEditorContent(string htmlSnippet) 
+        {
+            var html = htmlSnippet;
+            if (!string.IsNullOrEmpty(html))
+            {
+                html = HttpUtility.UrlDecode(html);
+            }
+
+            //HttpCookie editorCookie = new HttpCookie("editorSnippet");
+            //editorCookie.Value = html;
+            //editorCookie.Expires = now.AddMinutes(10);
+            //Response.Cookies.Add(editorCookie);
+
+            var jsonObject = new { IsSuccess = false, ErrorMessage = "", ReturnData = "" };
+            jsonObject = new { IsSuccess = true, ErrorMessage = "", ReturnData = "" };
+            return Json(jsonObject);
         }
 
         public ActionResult Index()
