3d0716278408165cce18f5154b11660230baa707
summertang <summer.tang@spring-trees.com>
2014-10-14 16:17:58 +0800

    #5 ckeditor

--- cythilya/cythilya/Content/ui/ckeditor/config.js
+++ cythilya/cythilya/Content/ui/ckeditor/config.js
@@ -3,8 +3,22 @@
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+CKEDITOR.editorConfig = function (config) {
+    config.language = 'zh-tw';
+    config.uiColor = '#BFBFBF';
+    config.height = 300;
+    //config.contentsCss = ['/Content/layout.css', '/Content/html.css'];
+    config.toolbar_Full = [['Source', '-', 'Save', 'NewPage', 'Preview', '-', 'Templates'],
+                           ['Undo', 'Redo', '-', 'SelectAll', 'RemoveFormat'],
+                           ['Styles', 'Format', 'Font', 'FontSize'],
+                           ['TextColor', 'BGColor'],
+                           ['Maximize', 'ShowBlocks', '-', 'About'], '/',
+                           ['Bold', 'Italic', 'Underline', 'Strike', '-', 'Subscript', 'Superscript'],
+                           ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'Blockquote', 'CreateDiv'],
+                           ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+                           ['Link', 'Unlink', 'Anchor'],
+                           ['Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak'],
+                           /*['Code']*/];
+    /*config.extraPlugins = 'CodePlugin';*/
+    config.allowedContent = true;
 };
