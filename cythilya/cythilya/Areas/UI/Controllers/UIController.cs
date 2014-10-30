a70e89db02af751ead31ae1986c8bf83e40ed183
Cythilya Tang <cythilya@gmail.com>
2014-10-30 22:32:51 +0800

    AngularJS

--- cythilya/cythilya/Areas/UI/Controllers/UIController.cs
+++ cythilya/cythilya/Areas/UI/Controllers/UIController.cs
@@ -55,7 +55,7 @@ namespace cythilya.Areas.UI.Controllers
 
         #endregion
 
-        #region AngularJS
+        #region AngularJS CRUD - Todo List
         public ActionResult AngularJSTodoList() 
         {
             return View();
