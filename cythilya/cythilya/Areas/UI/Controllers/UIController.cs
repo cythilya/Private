8ab612eb1ecc2bdc764f002a1a696c184d18facf
Cythilya Tang <cythilya@gmail.com>
2014-10-30 22:30:12 +0800

    AngularJS

--- cythilya/cythilya/Areas/UI/Controllers/UIController.cs
+++ cythilya/cythilya/Areas/UI/Controllers/UIController.cs
@@ -8,6 +8,8 @@ namespace cythilya.Areas.UI.Controllers
 {
     public class UIController : Controller
     {
+        #region CKEditor
+
         public ActionResult CKEditorPreview() 
         {
             //get saved html snippet
@@ -42,100 +44,22 @@ namespace cythilya.Areas.UI.Controllers
             return Json(jsonObject);
         }
 
+        #endregion
+
+        #region D3.js
+
         public ActionResult d3SimpleBarChart()
         {
             return View();
         }
 
-        public ActionResult Index()
+        #endregion
+
+        #region AngularJS
+        public ActionResult AngularJSTodoList() 
         {
             return View();
         }
-
-        //
-        // GET: /UI/UI/Details/5
-
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        //
-        // GET: /UI/UI/Create
-
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        //
-        // POST: /UI/UI/Create
-
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //
-        // GET: /UI/UI/Edit/5
-
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        //
-        // POST: /UI/UI/Edit/5
-
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //
-        // GET: /UI/UI/Delete/5
-
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        //
-        // POST: /UI/UI/Delete/5
-
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        #endregion
     }
 }
