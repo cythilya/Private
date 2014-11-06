a60ed31bb618fe82282e364ea4d932900f98592e
Cythilya Tang <cythilya@gmail.com>
2014-11-07 00:00:50 +0800

    project: emperorlove

--- cythilya/cythilya/Controllers/MeController.cs
+++ cythilya/cythilya/Controllers/MeController.cs
@@ -816,7 +816,7 @@ namespace cythilya.Controllers
             proj_9.Name = "Hello! Mip x Friendo 粉多任務";
             proj_9.Description = "Hello! Mip與粉多任務合作的活動網站。";
             proj_9.Role = "Front-End Development";
-            proj_9.Tech = "Web Development with SCSS and jQuery ";
+            proj_9.Tech = "Web Development with SCSS and jQuery";
             proj_9.URL = "/Me/Project?id=9";
             proj_9.RawPic = "/Content/me/img/project/hellomip/hellomip_index.png";
             proj_9.PicSmall = "/Content/me/img/project/hellomip/hellomip_index_188x73.png";
@@ -910,6 +910,40 @@ namespace cythilya.Controllers
 
             proj_11.Snapshot = SnapshotList11;
             projList.Add(proj_11);
+
+            #endregion
+
+            #region 御奉小餐館
+            MeModels.Project proj_12 = new MeModels.Project();
+            proj_12.ID = 12;
+            proj_12.Name = "御奉小餐館 x Friendo 粉多任務";
+            proj_12.Description = "御奉小餐館與粉多任務合作的活動網站。";
+            proj_12.Role = "Facebook Social Plugins and Graph API技術支援。";
+            proj_12.Tech = "Web Development with SCSS and jQuery";
+            proj_12.URL = "/Me/Project?id=12";
+            proj_12.RawPic = "/Content/me/img/project/emperorlove/emperorlove.png";
+            proj_12.PicSmall = "/Content/me/img/project/emperorlove/emperorlove_188x73.png";
+            proj_12.PicMedium = "/Content/me/img/project/emperorlove/emperorlove_300.png";
+            proj_12.PicLarge = "/Content/me/img/project/emperorlove/emperorlove_940x367.jpg";
+            proj_12.PicInFooter = "/Content/me/img/project/emperorlove/emperorlove_54x54.png";
+            proj_12.Order = 12;
+            proj_12.Client = "御奉小餐館";
+            proj_12.ClientURL = "http://www.emperorlove.com.tw";
+            proj_12.Date = "Oct 2014";
+            proj_12.LauchURL = "http://www.friendo.com.tw/emperorlove";
+            proj_12.HtmlContent = "<p>《御奉小餐館emperorlove》心機美食，挑起姊妹間的鬥爭！後宮甄嬛傳不夠看，「後宮爭餅傳」才精彩！到底是什麼樣的特色美食，讓平常毫無心機的姊妹淘，也開始有心機了？且來看看御奉小餐館的心機美食，如可引起姊妹間的爭食搶奪，快邀姊妹淘來一嚐究竟，一起心機過招！參加活動即有四人同行一人我請優惠喔！</p>";
+            proj_12.Tag = "Web Brand";
+            proj_12.isHighlight = true;
+
+            List<MeModels.SnapshotInfo> SnapshotList12 = new List<MeModels.SnapshotInfo>();
+            MeModels.SnapshotInfo proj_12_snap_1 = new MeModels.SnapshotInfo();
+
+            proj_12_snap_1.Name = "Self TV";
+            proj_12_snap_1.SnapshotURL = "/Content/me/img/project/emperorlove/emperorlove.png";
+            SnapshotList12.Add(proj_12_snap_1);
+
+            proj_12.Snapshot = SnapshotList12;
+            projList.Add(proj_12);
 
             #endregion
 
