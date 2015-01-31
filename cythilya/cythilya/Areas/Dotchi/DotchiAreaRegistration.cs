c7f734e1e71cc2055e512d305f7b395abd57821d
Cythilya Tang <cythilya@gmail.com>
2015-01-31 08:43:43 +0800

    project dochi

---
+++ cythilya/cythilya/Areas/Dotchi/DotchiAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace cythilya.Areas.Dotchi
+{
+    public class DotchiAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Dotchi";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Dotchi_default",
+                "{controller}/{action}/{id}",
+                new {  action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
