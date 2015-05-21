811b49874f84dd253d635d0c05e36559795cc75e
Cythilya Tang <cythilya@gmail.com>
2015-05-21 19:29:44 +0800

    ebook

--- cythilya/cythilya/Content/file/ebook_20150520/js/mobile.js
+++ cythilya/cythilya/Content/file/ebook_20150520/js/mobile.js
@@ -96,6 +96,23 @@
 
             //touch event init
             function touchInit(){
+                /*
+                $('.container').on('touchmove touchstart touchend touchcancel',function(e){
+                    if(e.type == 'touchmove'){
+                        alert('touchmove');
+                    }
+                    if(e.type == 'touchstart'){
+                        alert('touchstart');
+                    }
+                    if(e.type == 'touchend'){
+                        alert('touchend');
+                    }
+                    if(e.type == 'touchcancel'){
+                        alert('touchcancel');
+                    }                                                            
+                });
+                */
+
                 $(document).on('touchmove',function(e){
                     e.preventDefault();
                 });
@@ -120,7 +137,8 @@
                     touchInit();
                     if($(window).innerWidth()>$(window).innerHeight()){
                         //橫
-                        dMask.show();
+                       //dMask.show();
+                       dMask.hide(); 
                     }else{
                         //直
                         dMask.hide();
